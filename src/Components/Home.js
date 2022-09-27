c02f043d9272bb9563c44e792c3715623cbc5c58
Sai <saiharshith987@gmail.com>
2022-09-27 08:28:07 +0530

    error

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,19 +1,30 @@
 import React, { useState } from "react";
 export const Home = ({ setType }) => {
   const [city, setCity] = useState('');
+  const [error, setError] = useState('');
   const [data, setData] = useState({});
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!city) {
       return;
     }
-    console.log(city);
     const cityName = city[0].toUpperCase() + city.substring(1);
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=286f97aee2d116992ce709789f047e39`);
-    const resdata = await res.json();
-    setData(resdata)
-    setType(resdata.weather[0].main)
-    setCity('');
+    try {
+      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=286f97aee2d116992ce709789f047e39`);
+      if (res.ok) {
+        const resdata = await res.json();
+        setData(resdata)
+        setType(resdata.weather[0].main)
+        setCity('');
+      } else {
+        const err = await res.json();
+        throw new Error(err.message || err.statusText);
+      }
+    }
+    catch (err) {
+      setError("City not found!")
+    }
+
   }
 
   return (
@@ -24,7 +35,7 @@ export const Home = ({ setType }) => {
         </form>
       </div>
       {
-        Object.keys(data).length > 0 && <div className="container">
+        Object.keys(data).length > 0 ? <div className="container">
           <div className="top">
             <div className="location">
               <p>{data.name}</p>
@@ -50,7 +61,8 @@ export const Home = ({ setType }) => {
               <p>Wind Speed</p>
             </div>
           </div>
-        </div>
+        </div> :
+          <div><h2 align="center">{error}</h2></div>
       }
     </>
   )
